78cc36ee1bd1b414a11de7de0ca96cf0f727aa64
mjota <mj720@ic.ac.uk>
2021-04-06 11:22:54 +0100

    added dense=True for K_approx

--- Nystrom_work/nystrom_numpy.py
+++ Nystrom_work/nystrom_numpy.py
@@ -36,7 +36,7 @@ class Nystrom(GenericNystrom):
         Returns
             K = Nystrom approximation to kernel'''
     
-        K_nq = self._pairwise_kernels(x, self.components_)
+        K_nq = self._pairwise_kernels(x, self.components_, dense=True)
         K_q_inv = self.normalization_.T @ self.normalization_
         K_approx = K_nq @ K_q_inv @ K_nq.T
         return aslinearoperator(K_approx) 
