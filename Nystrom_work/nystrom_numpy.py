30e3291fd77f3e421de854ec569292bcb33f2d99
yc7620 <yaniel.cabrera20@imperial.ac.uk>
2021-04-14 11:25:01 +0300

    numpy version now handles subsampling by specifying eignvalues interval

--- Nystrom_work/nystrom_numpy.py
+++ Nystrom_work/nystrom_numpy.py
@@ -4,10 +4,10 @@ from scipy.linalg import svd, eigh
 from scipy.sparse.linalg import aslinearoperator
 
 from nystrom_common import GenericNystrom 
-from numpy_utils import numpytools
+from numpy_utils import numpytools 
 from pykeops.numpy import LazyTensor
 
-from typing import Tuple
+from typing import Tuple, List
 
 class Nystrom(GenericNystrom):
     '''Nystrom class to work with Numpy arrays'''
@@ -15,19 +15,31 @@ class Nystrom(GenericNystrom):
     def __init__(self, n_components=100, kernel='rbf', sigma:float = None,
                  eps:float = 0.05, mask_radius:float = None, k_means = 10, 
                  n_iter:int = 10, inv_eps:float = None,
-                  verbose = False, random_state=None, tools = None):
+                  verbose = False, random_state=None, eigvals:List[int]=None):
 
+        '''
+        Args:
+            eigvals = eigenvalues index interval [a,b] for constructed K_q,
+             where 0 <= a < b < length of K_q
+            
+        '''
         super().__init__(n_components, kernel, sigma, eps, mask_radius,
                          k_means, n_iter, inv_eps, verbose, random_state)
         
         self.tools = numpytools
         self.LazyTensor = LazyTensor
+        self.eigvals = eigvals
+
+        if eigvals:
+            assert eigvals[0] < eigvals[1], 'eigvals = [a,b] needs a < b'
+            assert eigvals[1] < n_components, 'max eigenvalue index needs to be less\
+            than size of K_q = n_components'
 
     def _decomposition_and_norm(self, X:np.array) -> np.array:
         '''Computes K_q^{-1/2}'''
 
-        X = X + np.eye(X.shape[0], dtype=self.dtype)*self.inv_eps
-        S,U = eigh(X)
+        X = X + np.eye(X.shape[0], dtype=self.dtype)*self.inv_eps        
+        S,U = eigh(X, eigvals=self.eigvals)
         S = np.maximum(S, 1e-12)
         
         return np.dot(U / np.sqrt(S), U.T)
